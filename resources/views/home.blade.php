1d15eab5d1d2300d43df0d67e709a5a5b94e7357
SriAliaRosidah <sri.rosidah@students.paramadina.ac.id>
2023-05-29 23:40:28 +0700

    menambahkan valet dan halaman

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+        <meta http-equiv="X-US-Compatible" content="ie=edge">
+        <title>Personal Assistant</title>
+    </head>
+    <body>
+        <h1>Halaman Home</h1>
+    </body>
+</html>
